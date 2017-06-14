418bf8a9e6c31f317fbd278b190f67da4ea31fde
Server <server@server.com>
2017-06-14 19:00:36 +0000

    AJAX handling

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
+var r = require('rethinkdb');
+
 var express = require('express');
 var router = express.Router();
 
+var bodyParser = require('body-parser');
+var connection;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Faharu' });
