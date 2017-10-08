9976cc30f33046e80158d3687d21be044974fe06
root <root@v20379.vps.ag>
2017-10-08 07:57:59 +0000

    Messaging almost complete, need to test and then remove self messaging; addNode updated; no more sessions

--- routes/save.js
+++ routes/save.js
@@ -3,94 +3,107 @@ var _ = require('lodash');
 
 var express = require('express');
 var router = express.Router();
-var session = require('express-session');
+// var session = require('express-session');
 var tables = ['userLocation', 'user']
 
 const io = require('socket.io')(8000);
 /*var session = {
 	secret: 'faharu',
 	cookie: {}
-}
-router.use(session(session));*/
+}*/
+// router.use(session(session));
 
-var currentID = null;
-session.currentID = null;
-console.log(session.currentID);
+// var currentID = null;
+// session.currentID = null;
+// console.log(session.currentID);
 
 io.on('connection', (socket) => {
   // socket.emit('node', { hello: 'world' });
-  socket.on('addNode', (node) => {
+  socket.on('addNode', (node, callback) => {
     console.log('Socket open: ', node.timestamp);
-    save(node);
-  });
-
-  socket.on('getUsers', (user) => {
-    console.log(user);
-    getUsers(user, (err, userList) => {
-    	socket.emit('receiveUsers', {userList});
+    save(node, (err, pathID) => {
+    	if (err) throw err;
+    	callback(pathID);
     });
   });
 
-  socket.on('getMessages', (selectedRecipient) => {
-    console.log(selectedRecipient);
-    getMessages(selectedRecipient, (err, messageBuffer) => {
-    	socket.emit('messageBuffer', {messageBuffer});
-    });
-  });
+/*  socket.on('getUsers', (user, ack) => {
+    console.log('getUsers:', user);
+    console.log('ack', ack)
+    if (typeof user !== 'undefined' && user !== null) {
+	    getUsers(user, (err, userList) => {
+	    	if (err) throw err;
+	    	socket.emit('receiveUsers', {userList});
+	    	// console.log('userList:', userList)//why is this firing so much?
+	    });
+    } else {
+    	console.log('user undefined? ', user)
+    }
+  });*/
 
-  socket.on('sendMessage', (messageObj) => {
-    console.log('Message received, saving: ', messageObj.message);
-    upsertMessage(messageObj, (err, result) => {
+  socket.on('getUsers', (u, callback) => {
+	getUsers(u, (err, userList) => {
+		if (err) throw err;
+		// socket.emit('receiveUsers', {userList});
+		callback({userList: userList});
+	});
+  })
+
+/*  socket.on('getMessages', (data) => {
+    console.log(data.selectedRecipient);
+    getMessages(data.selectedRecipient, (err, messageBuffer) => {
+    	socket.emit('receiveMessages', {messageBuffer});
+    });
+  });*/
+
+  socket.on('sendMessage', (data, callback) => {
+    console.log('Message received, saving: ', JSON.stringify(data));
+    upsertMessage(data, (err, result) => {
     	if (err) throw err;
     	console.log('upsertMessage result: ', result);
+    	callback(true)
     });
-    // getMessages(messageObj.users, (err, messageBuffer) => {
-    // 	socket.emit('messageBuffer', {messageBuffer});
-    // })
   });
+
+  socket.on('loadMessages', (data, callback) => {
+  	console.log('users[]', data.users)
+  	getMessages(data.users, (err, res) => {
+  		if (err) throw err;
+  		callback(null, res);
+  	});
+  })
 
 });
 
-upsertMessage = (messageObj, callback) => {//upsert
+upsertMessage = (message, callback) => {//upsert
 	r.connect({db: 'vedi'}, (err, conn) => {
 		if (err) callback(err);
-		// getMessages(messageObj.users, (err, messageID) => {
+		// getMessages(newMessage.users, (err, messageID) => {
 		// if (err) callback(err);
-		r.table('messaging').filter({'users': messageObj.users}).pluck('id').run(conn, (err, result) => {
-			if (err) callback (err);
-			if (typeof result !== 'undefined' && result !== null && result > 0) {
-				r.table('messaging').get(result).update({
-					messages: r.row('messages').append({
-						sender : messageObj.sender,
-						recipient : messageObj.recipient,
-						messageBody : messageObj.message,
-						timestamp : messageObj.timestamp,
-					}).run(conn, (err, r) => {
-						if (err) callback(err);
-						console.log('append: ', r);
-						callback(null, r);
-					})
-				});
-			} else {
-				r.table('messaging').insert(messageObj).run(conn, (err, r) => {
+		r.table('messaging').filter({users: message.users}).count().gt(0).run(conn, (err, result) => {
+			if (err) callback(err);
+			if (result) {
+				r.table('messaging').filter((user) => {
+					return user('users').eq(message.users);
+				}).update({
+					messages: r.row('messages').append(message.message)
+				}).run(conn, (err, r) => {
 					if (err) callback(err);
-					console.log('insert:', r);
+					console.log('append: ', r);
 					callback(null, r);
+				});				
+			} else {
+				r.table('messaging').insert({
+					users: message.users,
+					messages: [message.message],
+				}).run(conn, (err, r) => {
+					if (err) callback(err);
+					console.log('insert:', message.message.messageBody);
+					callback(null, message.message.messageBody);
 				})
-				// callback(result);
-			}
+			}	
 		})
-		// if ( typeof messageID !== 'undefined' && messageID !== null ) {//update/append
-			// r.table('messaging').run(conn, (err, result) => {
-				// if (err) callback(err);
-				
-			// });
-		// } else {//insert
-			// console.log('messageID: ', messageID);
-			
-		// }
-		console.log('Saved message: ', messageObj.message)
-		// });
+		console.log('Saved message: ', message.message.messageBody)
 	});
 }
 
@@ -100,15 +113,25 @@ getMessages = (users, callback) => {
 		r.table('messaging').count().gt(0).run(conn, (err, result) => {
 			if (err) callback (err);
 			if (result) {
-				r.table('messaging').filter({users: users}).run(conn, (err, result) => {
+				r.table('messaging').filter({users: users}).run(conn, (err, result) => {//select where users are sender and recipient
 					if (err) callback(err);
 					// console.log('Messages...', result);
 					if (result) {
-						console.log(result.id);
-						callback(null, result.id);
+						result.toArray((err, r) => {//convert result to array with 1 value
+							if (err) callback(err);
+							if (r.length > 0) {//if array is not 0, a chat already exists
+								if (typeof r[0] !== 'undefined') {
+									console.log('getMessages result 1:', r[0]);
+									callback(null, r[0]);//sends back the message as an object, not an array
+								} else {
+									callback('r[0] undefined')
+								}
+								
+							}
+						});
 					} else {
-						console.log(result);
-						callback('Result is empty', null)
+						console.log('getMessages result 2: ', result);
+						callback(result)
 					}
 					// result.toArray((err, results) => {
 					// 	if (err) callback(err);
@@ -116,7 +139,8 @@ getMessages = (users, callback) => {
 					// })
 				})
 			} else {
-				callback('No records exist');
+				console.log('No records')
+				callback(result);
 			}
 		})
 	})
@@ -130,10 +154,10 @@ getUsers = (currentUser, callback) => {
 			cursor.toArray((err, results) => {
 				if (err) callback (err);
 				// console.log('Results:', results);
-				var result = [];
-				results.forEach((user) => {
-					result.push(user);
-				})
+				// var result = [];
+				// results.forEach((user) => {
+				// 	result.push(user);
+				// })
 				callback(null, results);
 			})
 		})
@@ -172,10 +196,11 @@ getUsers = (currentUser, callback) => {
 userLocationUpsert = (node, callback) => {
 	r.connect({db: 'vedi'}, (err, conn) => {
 		if (err) callback(err);
-		if (node.nodeNumber > 0 && typeof session.pathID !== 'undefined') {
+		if (typeof node.pathID !== 'undefined' && node.pathID !== null && node.nodeNumber !== 0) {
 			//if not nodeNumber #0, path exists, therefore append...
 			//append{///////////////////////////////////////////////////////////////////////////
-			r.table('userLocation').get(session.pathID).update({
+			console.log('pathID', node.pathID)
+			r.table('userLocation').get(node.pathID).update({
 				path: r.row("path").append({
 					nodeNumber: node.nodeNumber,
 					latitude: node.latitude,
@@ -184,9 +209,9 @@ userLocationUpsert = (node, callback) => {
 				})
 			}).run(conn, (err, r) => {
 				if (err) callback(err);
-				callback( null, session.pathID);//return the pathID that was just updated
+				callback( null, node.pathID);//return the pathID that was just updated
 			})
-		callback(null, session.pathID);
+		callback(null, node.pathID);
 		} else {
 			//if nodeNumber is 0, or path has not been created, insert...
 			//insert{///////////////////////////////////////////////////////////////////////////
@@ -250,18 +275,19 @@ userUpsert = (node, callback) => {
 	})
 }
 
-save = function (node) {
-	session.user = node.user.id;
+save = function (node, callback) {
+	// req.session.user = node.user.id;
 	userUpsert(node, (err, res) => {
 		if (err) console.log(err);
-		session.user = res;
-		console.log('User: ', session.user, '|| ', node.user.id);
+		// req.session.user = res;
+		console.log('User: ', node.user.id);
 	})
 	console.log('Inserting/Appending path');
 	userLocationUpsert(node, (err, res) => {
 		if (err) throw err;
-		session.pathID = res;
-		console.log('Node added to path: ', session.pathID)
+		// req.session.pathID = res;
+		callback(null, res)
+		console.log('Node added to path: ', res)
 	})
 }
 
